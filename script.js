cfc21e6c0032299356729355e94fd59981af9ecb
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-28 16:08:24 -0500

    adding degrees farenheight value to top card

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let selectedCityNameEl = document.getElementById("city__Placeholder");
 const apiKey = "a2f6fce88cfc4a69918f36922a1be74b";
 
 async function getWeatherData(city){
-  const url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+  const url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + apiKey;
 
   fetch(url)
   .then(function (response) {
@@ -54,13 +54,18 @@ function getSearchBarValue(event) {
 // use api to get specific data and display to html
 
 function accessWeatherData(data) {
-  
+  // getting name data and displaying 
  let nameValue = data.city.name 
  let nameText = document.createTextNode(nameValue)
  let addHeaderToDiv = selectedCityEl[0].appendChild(selectedCityNameEl);
+  addHeaderToDiv.appendChild(nameText);
 
- addHeaderToDiv.appendChild(nameText);
+  // getting Temperature data ~for the day~ and displaying 
+ let tempValue = data.list[0].main.temp 
+ let tempText = document.createTextNode(tempValue)
+ let addTempToDiv = selectedCityEl[0].appendChild(selectedCityTempEl)
 
+ addTempToDiv.appendChild(tempText)
  
 }
 
